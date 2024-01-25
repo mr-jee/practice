950f9ef820d28454eeb71ea452f5849cba12810f
mr-jee <navidaria333@gmail.com>
2024-01-25 03:40:58 +0330

    progress

--- Exercise Files/Ch 1/typecheck_start.py
+++ Exercise Files/Ch 1/typecheck_start.py
@@ -1,4 +1,4 @@
-# Python Object Oriented Programming by Joe Marini course example
+# Python Object-Oriented Programming by Joe Marini course example
 # Checking class types and instances
 
 
@@ -19,9 +19,15 @@ n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
 
 # TODO: use type() to inspect the object type
-
+print(type(b1))
+print(type(n1))
 
 # TODO: compare two types together
-
+print(type(b1) == type(b2))
+print(type(b1) == type(n2))
 
 # TODO: use isinstance to compare a specific instance to a known type
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(n2, Book))
+print(isinstance(n2, object))
